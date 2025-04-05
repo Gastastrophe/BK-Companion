3647955ab2c234123883bf2f972056e2f626f278
Evan <emiller9045@gmail.com>
2025-04-05 12:34:28 -0400

    Add Played Cards list
    Adds a list containing all played cards in a turn. Currently prints to terminal when the turn ends.

--- BK-Companion.py
+++ BK-Companion.py
@@ -6,16 +6,16 @@ from character import character
 from magnus import deck
 
 # Shuffle a spent deck
-def get_new_deck(hand, battle_deck, card_frames, message_frame):
+def get_new_deck(hand, battle_deck, card_frames, message_frame, played_cards):
     # Get a new deck
     battle_deck.cards = deck().cards
     random.shuffle(battle_deck.cards)
     # Draw cards equal to hand size
     hand = []
     for _ in range(player.hand_size):
-        draw_card(hand, battle_deck, card_frames, message_frame)
+        draw_card(hand, battle_deck, card_frames, message_frame, played_cards)
     # Update hand frame
-    update_hand(hand, battle_deck, card_frames, message_frame)
+    update_hand(hand, battle_deck, card_frames, message_frame, played_cards)
 
 # Displays a card in the center
 def display_card(played_card, message_frame):
@@ -46,17 +46,21 @@ def display_card(played_card, message_frame):
     message_frame.nametowidget("cardDetails").config(text = text)
 
 # Plays a card from your hand
-def play_card(i, hand, battle_deck, card_frames, message_frame):
+def play_card(i, hand, battle_deck, card_frames, message_frame, played_cards):
     # Add 1 to the combo counter
     combo_counter = message_frame.nametowidget("comboCounter")
     combo_counter.config(text = combo_counter.cget("text")[:-1] + str(int(combo_counter.cget("text")[-1]) + 1))
-    # Remove the played card from the hand and display it
-    display_card(hand.pop(i), message_frame)
+    # Remove the played card from the hand
+    card = hand.pop(i)
+    # Add the played card to the played card list
+    played_cards.append(card)
+    # Display the drawn card
+    display_card(card, message_frame)
     # Draw a new card
-    draw_card(hand, battle_deck, card_frames, message_frame)
+    draw_card(hand, battle_deck, card_frames, message_frame, played_cards)
 
 # Add cards from hand to card frames
-def update_hand(hand, battle_deck, card_frames, message_frame):
+def update_hand(hand, battle_deck, card_frames, message_frame, played_cards):
     # Remove all existing card widgets and create new ones for the updated hand
     for i in range(len(card_frames)):
         for widget in card_frames[i].winfo_children():
@@ -65,19 +69,19 @@ def update_hand(hand, battle_deck, card_frames, message_frame):
         # Make a name label, a display button and a play button
         card_name  = tk.Label(master=card_frames[i], text=card["name"], name="cardName" + str(i))
         card_name.pack()
-        play_button = tk.Button(master = card_frames[i], text="Play", command = partial(play_card, i, hand, battle_deck, card_frames, message_frame))
+        play_button = tk.Button(master = card_frames[i], text="Play", command = partial(play_card, i, hand, battle_deck, card_frames, message_frame, played_cards))
         play_button.pack()
         display_button = tk.Button(master = card_frames[i], text="Show", command = partial(display_card, card, message_frame))
         display_button.pack()
 
 # Draws a card from a deck
-def draw_card(hand, battle_deck, card_frames, message_frame):
+def draw_card(hand, battle_deck, card_frames, message_frame, played_cards):
     # If the deck is empty, tell the user to reshuffle
     if len(battle_deck.cards) == 0:
         deck_size = message_frame.nametowidget("deckSize")
         deck_size.config(text = deck_size.cget("text")[:6] + "Please shuffle the deck")
         # Update hand visuals
-        update_hand(hand, battle_deck, card_frames, message_frame)
+        update_hand(hand, battle_deck, card_frames, message_frame, played_cards)
     # Otherwise, draw a card
     else:
         # Draw a card
@@ -86,12 +90,16 @@ def draw_card(hand, battle_deck, card_frames, message_frame):
         deck_size = message_frame.nametowidget("deckSize")
         deck_size.config(text = deck_size.cget("text")[:6] + str(len(battle_deck.cards)))
         # Update hand visuals
-        update_hand(hand, battle_deck, card_frames, message_frame)
+        update_hand(hand, battle_deck, card_frames, message_frame, played_cards)
 
 # Reset the combo counter and display the damage report
-def end_turn(message_frame):
+def end_turn(message_frame, played_cards):
+    # Reset combo counter
     combo_counter = message_frame.nametowidget("comboCounter")
     combo_counter.config(text = combo_counter.cget("text").split(": ")[0] + ": " + str(0))
+    # Reset played cards
+    print(played_cards)
+    played_cards.clear()
 
 # Creates a GUI for battle
 def start_battle(player):
@@ -103,8 +111,10 @@ def start_battle(player):
     # Create a combo counter
     combo_counter = tk.Label(master=message_frame, text="Combo: 0", name="comboCounter")
     combo_counter.place(rely=0, relx=1.0, x=0, y=0, anchor="ne")
+    # Create a list for played cards
+    played_cards = []
     # Create an end turn button
-    turn_ender = tk.Button(master=message_frame, text="End Turn", name="endTurn", command = lambda: end_turn(message_frame))
+    turn_ender = tk.Button(master=message_frame, text="End Turn", name="endTurn", command = lambda: end_turn(message_frame, played_cards))
     turn_ender.place(rely=0, relx=0, x=0, y=0, anchor="nw")
     # Create an area to display card effects
     card_details = tk.Label(master=message_frame, text = "", name = "cardDetails")
@@ -125,12 +135,12 @@ def start_battle(player):
     combo_counter = tk.Label(master=message_frame, text="Deck: " + str(len(battle_deck.cards)), name="deckSize")
     combo_counter.place(rely=1.0, relx=1.0, x=0, y=0, anchor="se")
     # Create a shuffle deck button
-    deck_resetter = tk.Button(master=message_frame, text="Shuffle Deck", name="shuffleDeck", command = lambda: get_new_deck(hand, battle_deck, card_frames, message_frame))
+    deck_resetter = tk.Button(master=message_frame, text="Shuffle Deck", name="shuffleDeck", command = lambda: get_new_deck(hand, battle_deck, card_frames, message_frame, played_cards))
     deck_resetter.place(rely=1.0, relx=0, x=0, y=0, anchor="sw")
     # Draw cards equal to hand size
     hand = []
     for _ in range(player.hand_size):
-        draw_card(hand, battle_deck, card_frames, message_frame)
+        draw_card(hand, battle_deck, card_frames, message_frame, played_cards)
 
     # Update the window to update it
     window.mainloop()
